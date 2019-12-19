df33332bc5da2cdaab28a6a913bab74f12ed588f
custiom <custiom@naver.com>
2019-12-19 17:58:04 +0900

    Did Learn Basic Things for React, Props, State

---
+++ src/Movie.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import propTypes from  'prop-types';
+import './Movie.css';
+
+class Movie extends Component{
+
+    static propTypes = {
+        title : propTypes.string.isRequired,
+        poster : propTypes.string.isRequired
+    }
+
+    render(){
+        return(
+            <div>
+                <MoviePoster poster={this.props.poster}/>
+                <h1>{this.props.title}</h1>
+            </div>
+        );
+    }
+}
+
+class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: propTypes.string.isRequired
+    }
+
+    render(){
+        return(
+            <img src ={this.props.poster} alt="Movie Poster"/>
+        );
+    }
+}
+
+export default Movie;

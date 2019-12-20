533eddff627efe1f9358388f4407eada33387b8a
custiom <custiom@naver.com>
2019-12-20 11:05:07 +0900

    Class -> Function for Dump Component

--- src/App.js
+++ src/App.js
@@ -5,52 +5,26 @@ import Movie from './Movie.js';
 class App extends Component{
 
   state = {
-    greeting : 'Hello!',
-    movies : [
-      {
-        title : "Matrix",
-        poster : "https://miro.medium.com/max/20000/1*6YEZ5H5c5U-P5y_B4isT4w.jpeg"
-      },
-      {
-        title : "Full Metal Jacket",
-        poster : "https://t1.daumcdn.net/cfile/tistory/9915D6355CC497ED03"
-      },
-      {
-        title : "Oldboy",
-        poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7z7fPJ4CQETPsGc5jvt-dz3kuXhtMW4kJEgfjMv5zIAlBbZjiDw&s"
-      },
-      {
-        title : "Star Wars",
-        poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYTvDvmU9CdIlcdLFCeXxf-RwvvcMTIjD6SyoJNu0xOPlDY49g&s"
-      }
-    ]
   }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title : "transpotting",
-            poster : 'https://cdn.collider.com/wp-content/uploads/2015/09/trainspotting.jpg'
-          }
-          ...this.state.movies,
-        ]
-      });
-    }, 5000);
+    console.log(fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating'));
+  }
+
+  _renderMovies = () =>{
+    const movies = this.state.movies.map((movie, index) =>{
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    });
+    return movies;
   }
 
   render(){
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => (
-          <Movie title={movie.title} poster={movie.poster} key={index} />
-        ))}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
-
   
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -2,34 +2,29 @@ import React, {Component} from 'react';
 import propTypes from  'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-
-    static propTypes = {
-        title : propTypes.string.isRequired,
-        poster : propTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
-class MoviePoster extends Component{
 
-    static propTypes = {
-        poster: propTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src ={this.props.poster} alt="Movie Poster"/>
-        );
-    }
+function MoviePoster({poster}){
+    return(
+        <img src ={poster} alt="Movie Poster"/>
+    );
 }
+
+Movie.propTypes = {
+    title : propTypes.string.isRequired,
+    poster : propTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster : propTypes.string.isRequired
+};
 
 export default Movie;

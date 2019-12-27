039d71afa2dd6c0830b059c22fcb6dc025a2154b
custiom <custiom@naver.com>
2019-12-27 16:28:24 +0900

    Has taked the class
    1. I will Update it more perfect App

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,8 +2,17 @@ import React from "react";
 
 class Detail extends React.Component {
     
+    componentDidMount(){
+        console.log(this.props);
+        const {location, history} = this.props;
+        if(location.state === undefined){
+            history.push('/');
+        }
+    }
+
     render(){
-        return <span>Hellow</span>
+        const {location} = this.props;
+        return <span>{location.state.title}</span>
     }
     
 }
